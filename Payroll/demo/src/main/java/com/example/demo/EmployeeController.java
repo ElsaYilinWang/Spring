cbb4cdea62dc3508355e3bd33828242517cf2c4d
ElsaYilinWang <elsa.yilin.w39@gmail.com>
2024-06-11 20:23:49 +0100

    Update Employee Controller with assembler

--- Payroll/demo/src/main/java/com/example/demo/EmployeeController.java
+++ Payroll/demo/src/main/java/com/example/demo/EmployeeController.java
@@ -20,8 +20,13 @@ class EmployeeController{
 
     private final EmployeeRepository repository;
 
-    EmployeeController(EmployeeRepository repository){
+    // Injecting EmployeeModelAssembler
+    private final EmployeeModelAssembler assembler;
+
+
+    EmployeeController(EmployeeRepository repository, EmployeeModelAssembler assembler){
         this.repository = repository;
+        this.assembler = assembler;
     }
 
     // Aggregate root
@@ -31,9 +36,10 @@ class EmployeeController{
     CollectionModel<EntityModel<Employee>> all() {
 
     List<EntityModel<Employee>> employees = repository.findAll().stream()
-      .map(employee -> EntityModel.of(employee,
-          linkTo(methodOn(EmployeeController.class).one(employee.getId())).withSelfRel(),
-          linkTo(methodOn(EmployeeController.class).all()).withRel("employees")))
+      .map(assembler::toModel) // Getting aggregate root resource using the assembler
+        // employee -> EntityModel.of(employee,
+        //  linkTo(methodOn(EmployeeController.class).one(employee.getId())).withSelfRel(),
+        //  linkTo(methodOn(EmployeeController.class).all()).withRel("employees"))
       .collect(Collectors.toList());
 
      return CollectionModel.of(employees, linkTo(methodOn(EmployeeController.class).all()).withSelfRel());
@@ -57,10 +63,14 @@ class EmployeeController{
 
         Employee employee = repository.findById(id).orElseThrow(() -> new EmployeeNotFoundException(id));
 
-        return EntityModel.of(employee, //
-            linkTo(methodOn(EmployeeController.class).one(id)).withSelfRel(),
-            linkTo(methodOn(EmployeeController.class).all()).withRel("employees")
-        );
+
+        // Getting single item resource using the assembler
+        return assembler.toModel(employee);
+
+        // EntityModel.of(employee, //
+            // linkTo(methodOn(EmployeeController.class).one(id)).withSelfRel(),
+            // linkTo(methodOn(EmployeeController.class).all()).withRel("employees")
+        //);
     }
 
     
