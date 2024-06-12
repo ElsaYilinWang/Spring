e3bedcd9df12385b7fb175fa74442d8a2030603b
ElsaYilinWang <elsa.yilin.w39@gmail.com>
2024-06-12 12:14:06 +0100

    Update the DB prel-loader

--- Payroll/demo/src/main/java/com/example/demo/LoadDatabase.java
+++ Payroll/demo/src/main/java/com/example/demo/LoadDatabase.java
@@ -11,14 +11,28 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 class LoadDatabase{
+
     private static final Logger log = LoggerFactory.getLogger(LoadDatabase.class);
 
     @Bean
-    CommandLineRunner initDatabase(EmployeeRepository repository){
+    CommandLineRunner initDatabase(EmployeeRepository employeeRepository,//
+                OrderRepository orderRepository){
+
         return args -> {
-            // Update name to firstName, lastName
-            log.info("Preloading " + repository.save(new Employee("Bilbo", "Baggins", "burglar")));
-            log.info("Preloading " + repository.save(new Employee("Frodo", "Baggins", "thief")));
+            // Update employee repository and order repository
+            employeeRepository.save(new Employee("Bilbo", "Baggins", "burglar"));
+            employeeRepository.save(new Employee("Frodo", "Baggins", "thief"));
+
+            employeeRepository.findAll().forEach(employee -> log.info("Preloaded " + employee));
+
+            orderRepository.save(new Order("MacBook Pro", Status.COMPLETED));
+            orderRepository.save(new Order("iPhone", Status.IN_PROGRESS));
+
+
+            orderRepository.findAll().forEach( //
+                order -> {log.info("Preloading " + order);}
+                );
+            
         };
     }
 }
