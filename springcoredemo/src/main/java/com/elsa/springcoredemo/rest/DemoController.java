0f23cdc2575873b5f5daf2ac620181105ebdffd5
ElsaYilinWang <elsa.yilin.w39@gmail.com>
2024-06-25 20:06:21 +0100

    Spring core demo - Primary (only one!)

--- springcoredemo/src/main/java/com/elsa/springcoredemo/rest/DemoController.java
+++ springcoredemo/src/main/java/com/elsa/springcoredemo/rest/DemoController.java
@@ -4,7 +4,8 @@ import com.elsa.springcoredemo.common.Coach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.beans.factory.annotation.Qualifier;
+//import org.springframework.beans.factory.annotation.Qualifier;
+// Qualifier for more specific beans
 
 @RestController
 public class DemoController {
@@ -14,7 +15,8 @@ public class DemoController {
 
     // define a constructor for dependency
     @Autowired
-    public DemoController(@Qualifier("cricketCoach") Coach theCoach){
+    //public DemoController(@Qualifier("cricketCoach") Coach theCoach)
+    public DemoController(Coach theCoach){
         myCoach = theCoach;
     }
 
