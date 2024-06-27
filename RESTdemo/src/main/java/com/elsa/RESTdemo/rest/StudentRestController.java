09be3218f87f49b68be7b2e33440893c631e2a44
ElsaYilinWang <elsa.yilin.w39@gmail.com>
2024-06-27 17:00:52 +0100

    REST demo - Create a basic student list

---
+++ RESTdemo/src/main/java/com/elsa/RESTdemo/rest/StudentRestController.java
@@ -0,0 +1,29 @@
+package com.elsa.RESTdemo.rest;
+
+import com.elsa.RESTdemo.entity.Student;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/api")
+public class StudentRestController {
+
+    // define endpoint for "/students"
+    // return a list of students
+    @GetMapping("/students")
+    public List<Student> getStudents(){
+
+        List<Student> theStudents = new ArrayList<>();
+
+        theStudents.add(new Student("Poorina", "Patel"));
+        theStudents.add(new Student("Ron", "Wensley"));
+        theStudents.add(new Student("Harry", "Potter"));
+
+        return theStudents;
+    }
+}
