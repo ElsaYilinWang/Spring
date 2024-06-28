caa6bfcf43a86ba36f778358d616392e6fb1a02a
ElsaYilinWang <elsa.yilin.w39@gmail.com>
2024-06-28 18:59:56 +0100

    Spring Data REST, Configuration, Pagination and Sorting

--- RestCRUDemployees/src/main/java/com/elsa/RestCRUDemployees/dao/EmployeeRepository.java
+++ RestCRUDemployees/src/main/java/com/elsa/RestCRUDemployees/dao/EmployeeRepository.java
@@ -2,7 +2,9 @@ package com.elsa.RestCRUDemployees.dao;
 
 import com.elsa.RestCRUDemployees.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(path="members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
     // that's it ... no need to write any code LOL!
 }
