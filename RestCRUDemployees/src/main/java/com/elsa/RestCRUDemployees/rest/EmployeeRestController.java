d2cafb2296d8159b6c43fb2c2d36322a1dccf66f
ElsaYilinWang <elsa.yilin.w39@gmail.com>
2024-06-28 12:11:22 +0100

    REST CRUD Employee project - update and delete employees

--- RestCRUDemployees/src/main/java/com/elsa/RestCRUDemployees/rest/EmployeeRestController.java
+++ RestCRUDemployees/src/main/java/com/elsa/RestCRUDemployees/rest/EmployeeRestController.java
@@ -5,6 +5,7 @@ import com.elsa.RestCRUDemployees.entity.Employee;
 import com.elsa.RestCRUDemployees.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.service.annotation.DeleteExchange;
 
 import java.util.List;
 
@@ -50,6 +51,35 @@ public class EmployeeRestController {
 
         return dbEmployee;
     }
+
+    // add mapping for PUT /employees
+    // - update existing employee
+    @PutMapping("/employees")
+    public Employee updateEmployee(@RequestBody Employee theEmployee){
+
+        Employee dbEmployee = employeeService.save(theEmployee);
+
+        return dbEmployee;
+    }
+
+    // add mapping for DELETE /employees/{employeeId}
+    // - delete employee
+    @DeleteMapping("/employees/{employeeId}")
+    public String deleteEmployee(@PathVariable int employeeId){
+
+        Employee tempEmployee = employeeService.findById(employeeId);
+
+        // throw exception if null
+
+        if (tempEmployee == null){
+            throw new RuntimeException("Employee id not found - " + employeeId);
+        }
+
+        employeeService.deleteById(employeeId);
+
+        return "Delete employee id - " + employeeId;
+    }
+
 
 }
 
