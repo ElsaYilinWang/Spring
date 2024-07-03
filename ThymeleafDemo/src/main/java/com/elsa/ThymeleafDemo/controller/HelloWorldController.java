fd54da2911c2e38961622f6442658801314e6fb6
ElsaYilinWang <elsa.yilin.w39@gmail.com>
2024-07-03 15:24:45 +0100

    Thymeleaf Demo project - add data to the model

--- ThymeleafDemo/src/main/java/com/elsa/ThymeleafDemo/controller/HelloWorldController.java
+++ ThymeleafDemo/src/main/java/com/elsa/ThymeleafDemo/controller/HelloWorldController.java
@@ -1,7 +1,9 @@
 package com.elsa.ThymeleafDemo.controller;
 
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -18,4 +20,32 @@ public class HelloWorldController {
     public String processForm(){
         return "helloworld";
     }
+
+    // need a controller method to read form data
+    // and add data to the model
+    @RequestMapping("/processFormVersionTwo")
+    public String letsShoutDude(HttpServletRequest request, Model model){
+
+        // read the request parameter from the HTML form
+        String theName = request.getParameter("studentName");
+
+        // convert the data to all caps
+        theName = theName.toUpperCase();
+
+        // create the message
+        String result = "Yo! " + theName;
+
+        // add message tp the model
+        model.addAttribute("message", result);
+
+        return "helloworld";
+    }
 }
+
+
+
+
+
+
+
+
