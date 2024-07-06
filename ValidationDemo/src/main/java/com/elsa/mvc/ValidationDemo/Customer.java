1bdc3dd746a3c54369982d678afadf39ace03e3e
ElsaYilinWang <elsa.yilin.w39@gmail.com>
2024-07-06 23:35:23 +0100

    Spring MVC - Number Range

--- ValidationDemo/src/main/java/com/elsa/mvc/ValidationDemo/Customer.java
+++ ValidationDemo/src/main/java/com/elsa/mvc/ValidationDemo/Customer.java
@@ -1,5 +1,7 @@
 package com.elsa.mvc.ValidationDemo;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
@@ -11,8 +13,20 @@ public class Customer {
     @Size(min=1, message="is required")
     private String lastName = "";
 
+    @Min(value=0, message="must be greater than or equal to zero")
+    @Max(value=10, message="must be less than or equal to 10")
+    private int freePasses;
+
     public Customer(){
 
+    }
+
+    public int getFreePasses() {
+        return freePasses;
+    }
+
+    public void setFreePasses(int freePasses) {
+        this.freePasses = freePasses;
     }
 
     public String getFirstName() {
