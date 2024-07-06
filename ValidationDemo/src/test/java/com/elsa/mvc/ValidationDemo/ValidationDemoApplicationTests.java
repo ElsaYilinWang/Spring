dbba7c48a4ad5f47e4d4bb9dd1fac2903178409c
ElsaYilinWang <elsa.yilin.w39@gmail.com>
2024-07-06 23:26:54 +0100

    Spring MVC - InitBinder

---
+++ ValidationDemo/src/test/java/com/elsa/mvc/ValidationDemo/ValidationDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elsa.mvc.ValidationDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValidationDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
