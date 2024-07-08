99436743ffa9eeda6b115cd79dedacab9825f8fc
ElsaYilinWang <elsa.yilin.w39@gmail.com>
2024-07-08 17:04:48 +0100

    Spring MVC Security - LogOut page

--- MVCSecurity/src/main/java/com/elsa/MVCSecurity/security/DemoSecurityConfig.java
+++ MVCSecurity/src/main/java/com/elsa/MVCSecurity/security/DemoSecurityConfig.java
@@ -48,8 +48,8 @@ public class DemoSecurityConfig {
                                 .loginProcessingUrl("/authenticateTheUser")
                                 .permitAll()
 
-
-        );
+        )
+                .logout(logout -> logout.permitAll());
 
         return http.build();
     }
