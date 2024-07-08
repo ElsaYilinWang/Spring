ee2f77487da05daeaa2d68a6fc67341ba03ac74b
ElsaYilinWang <elsa.yilin.w39@gmail.com>
2024-07-08 13:29:09 +0100

    Spring MVC Security - starter code

---
+++ MVCSecurity/src/test/java/com/elsa/MVCSecurity/MvcSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elsa.MVCSecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
