67676a7516244b3514fb0b205fff3910d7ef9388
ElsaYilinWang <elsa.yilin.w39@gmail.com>
2024-07-09 14:54:03 +0100

    JPA Advanced Mapping - find and delete instructors

--- AdvancedMapping/src/main/java/com/elsa/AdvancedMapping/dao/AppDAO.java
+++ AdvancedMapping/src/main/java/com/elsa/AdvancedMapping/dao/AppDAO.java
@@ -4,4 +4,8 @@ import com.elsa.AdvancedMapping.entity.Instructor;
 public interface AppDAO {
 
     void save(Instructor theInstructor);
+
+    Instructor findInstructorById(int theId);
+
+    void deleteInstructorById(int theId);
 }
