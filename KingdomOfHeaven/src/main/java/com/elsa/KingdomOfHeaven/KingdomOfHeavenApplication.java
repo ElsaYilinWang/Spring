802877a19ff3b95f9671df9cc3130d1a1284bf69
ElsaYilinWang <elsa.yilin.w39@gmail.com>
2024-07-10 10:56:27 +0100

    Kingdom Of Heaven starter code

---
+++ KingdomOfHeaven/src/main/java/com/elsa/KingdomOfHeaven/KingdomOfHeavenApplication.java
@@ -0,0 +1,13 @@
+package com.elsa.KingdomOfHeaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KingdomOfHeavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KingdomOfHeavenApplication.class, args);
+	}
+
+}
---
+++ KingdomOfHeaven/src/test/java/com/elsa/KingdomOfHeaven/KingdomOfHeavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elsa.KingdomOfHeaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KingdomOfHeavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
