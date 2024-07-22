3679847a38229ff40a75d8d847618f3bdcb02214
ElsaYilinWang <elsa.yilin.w39@gmail.com>
2024-07-22 23:21:55 +0100

    MongoDB springboot: addExpense & getAllExpenses

--- MongoDB-SpringBoot/src/main/java/com/elsa/MongoDB_SpringBoot/controller/ExpenseController.java
+++ MongoDB-SpringBoot/src/main/java/com/elsa/MongoDB_SpringBoot/controller/ExpenseController.java
@@ -1,9 +1,15 @@
 package com.elsa.MongoDB_SpringBoot.controller;
 
+import com.elsa.MongoDB_SpringBoot.model.Expense;
 import com.elsa.MongoDB_SpringBoot.service.ExpenseService;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.apache.coyote.Response;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/expense")
@@ -11,4 +17,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class ExpenseController {
 
     private final ExpenseService expenseService;
+
+    @PostMapping
+    public ResponseEntity addExpense(@RequestBody Expense expense){
+        expenseService.addExpense(expense);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Expense>> getAllExpenses(){
+
+        return ResponseEntity.ok(expenseService.getAllExpenses());
+    }
 }
--- MongoDB-SpringBoot/src/main/java/com/elsa/MongoDB_SpringBoot/service/ExpenseService.java
+++ MongoDB-SpringBoot/src/main/java/com/elsa/MongoDB_SpringBoot/service/ExpenseService.java
@@ -1,12 +1,26 @@
 package com.elsa.MongoDB_SpringBoot.service;
 
+import com.elsa.MongoDB_SpringBoot.model.Expense;
 import com.elsa.MongoDB_SpringBoot.repository.ExpenseRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class ExpenseService {
 
     private final ExpenseRepository expenseRepository;
+
+    public void addExpense(Expense expense){
+
+        expenseRepository.insert(expense);
+    }
+
+    public List<Expense> getAllExpenses(){
+        return expenseRepository.findAll();
+    }
 }
