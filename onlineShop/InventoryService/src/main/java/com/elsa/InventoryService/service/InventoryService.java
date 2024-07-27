53752b4908f7c4b8e25e746c16cbbe85ed3214a5
ElsaYilinWang <elsa.yilin.w39@gmail.com>
2024-07-27 17:38:25 +0100

    Distributed Tracing: Spring Cloud Sleuth & Zipkin dependency

--- onlineShop/InventoryService/src/main/java/com/elsa/InventoryService/service/InventoryService.java
+++ onlineShop/InventoryService/src/main/java/com/elsa/InventoryService/service/InventoryService.java
@@ -19,7 +19,11 @@ public class InventoryService {
     @Transactional(readOnly = true)
     @SneakyThrows
     public List<InventoryResponse> isInStock(List<String> skuCode){
-        log.info("Checking INventory");
+        log.info("Checking Inventory");
+
+        log.info("Wait started.");
+        Thread.sleep(10000);
+        log.info("Wait ended.");
 
         return inventoryRepository.findBySkuCodeIn(skuCode)
                 .stream()
